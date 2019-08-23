f1ed6ae2c8eecc5ea58a5d0e05b3589c39cdf60f
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-08-23 13:03:00 +0300

    mission
    Изменил mission

---
+++ script.js
@@ -0,0 +1,8 @@
+let money = 100; 
+let income = 1000;
+let addExpenses = 750;
+let deposit = 50;
+let mission = 16000; 
+let period = 1.5;
+alert('У меня есть ' + money + 'и скоро придет моя з/п ' + income);
+console.log('Но, если по секрету, я все пробухаю))');
