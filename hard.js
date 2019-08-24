a2f7675f658b2c851f8f9c58d38d2cf307a37d37
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-08-24 15:12:25 +0300

    Усложненка
    Сделал усложненку во втором задании

---
+++ hard.js
@@ -0,0 +1,10 @@
+let num = 266219;
+var a = 1;
+while (num > 0) {
+    a = a * (num % 10);
+    num = Math.floor(num/10);
+}
+console.log("result: " + a);
+a = a**3
+let myVar = '' + a
+console.log(myVar.substring(0,2));
