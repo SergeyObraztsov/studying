3848082ba459b0a90caef15badf3b9806080f0e4
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-08-28 23:24:23 +0300

    перенес домашку в нужную папку

--- lesson04/script.js
+++ lesson04/script.js
@@ -3,14 +3,7 @@
 let mission = 160000; 
 let money = prompt('Ваш месячный доход?');
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log("addExpenses: ", addExpenses.split(', '));
 let deposit = confirm('Есть ли у вас депозит в банке?');
-
-
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-
 
 let expenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
 let price1 = prompt('Во сколько это обойдется?');
@@ -25,10 +18,26 @@ Math.floor(budgetDay)
 let happyness = mission / budgetMonth
 Math.ceil(happyness)
 
-if (budgetDay >= 800) console.log('Высокий уровень дохода');
-else if (budgetDay >= 300) console.log('Средний уровень дохода');
-else if (budgetDay >= 0) console.log('низкий уровень дохода');
-else console.log('something went wrong');
+let detStatusIncome = function(){
+    if (budgetDay >= 800) {
+        return ('Высокий уровень дохода');
+    } else if (budgetDay >= 300) {
+        return ('Средний уровень дохода');
+    } else if (budgetDay >= 0) {
+        return ('низкий уровень дохода');
+    } else {
+        return ('something went wrong');
+    }
+}
+
+
+const showTypeof = function(data){
+    console.log(data, typeof(data));
+}
+
+const getStatusIncome = function(){
+
+}
 
 const getExpensesMonth = function(){
     return (price1 + price2);
@@ -40,7 +49,7 @@ const getAccumulatedMonth = function(){
 }
 
 const getTargetMonth = function(){
-    return (mission / accumulatedMonth);
+    return (Math.floor(mission / accumulatedMonth));
 }
 console.log(getExpensesMonth());
 console.log(getAccumulatedMonth());
