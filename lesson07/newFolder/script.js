5ee79c75212310e1ba6cb5fa75a6029d37379af7
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-09-05 01:22:11 +0300

    домашка 7 урок
    доделал

---
+++ lesson07/newFolder/script.js
@@ -0,0 +1,26 @@
+'use strict';
+let score = document.getElementById('start'),
+    plusBtn1 = document.getElementsByTagName('button'),
+    cBox = document.querySelector('#deposit-check'),
+    polya = document.querySelectorAll(".result-additional_income"),
+    budgetMonthV = document.querySelector(".budget_month-value"),
+    budgetDayV = document.querySelector(".budget_day-value"),
+    expensesMonthV = document.querySelector(".expenses_month-value"),
+    incomeV = document.querySelector(".additional_income-value"),
+    enxpensesV = document.querySelector(".additional_expenses-value"),
+    incomePeriod = document.querySelector(".income_period-value"),
+    targetMonth = document.querySelector(".target_month-value"),
+    budget = document.querySelector(".salary-title"),
+    income = document.querySelector(".income-title"),
+    incomeCash = document.querySelector(".additional_income-title"),
+    addIncome = document.querySelector(".expenses-title"),
+    expenses = document.querySelector(".additional_expenses-title"),
+    expensesCash = document.querySelector(".expenses-amount"),
+    addExpenses = document.querySelector(".additional_expenses-title"),
+    deposit = document.querySelector(".deposit-label");
+
+    
+    
+    
+    
+    
