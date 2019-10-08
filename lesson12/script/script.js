1f8fd13bcad6b7b71b8661591fbc5be502e96148
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-10-09 01:27:10 +0300

    urok 14

--- lesson12/script/script.js
+++ lesson12/script/script.js
@@ -42,6 +42,101 @@ window.addEventListener('DOMContentLoaded', function(){
         updateClock();
     }
 
-    countTimer('25 september 2019');
+    countTimer('25 october 2019');
+
+    //menu
+    const toggleMenu = () => {
+        const btnMenu = document.querySelector(".menu"),
+            menu = document.querySelector('menu');
+
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+        btnMenu.addEventListener('click', handlerMenu);
+
+        menu.addEventListener('click', (event) => {
+            let target = event.target;
+            
+            if(event.target.matches('a')){
+                handlerMenu();
+            } else {
+                if(target.classList.contains('close-btn')){
+                    handlerMenu();
+                }
+            }
+        });
+
+    };
+    toggleMenu();
+
+    //popup
+
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn');
+        
+        popupBtn.forEach((elem) => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+            });
+        });
+
+        popup.addEventListener('click', (event) => {
+            let target = event.target;
+
+            if(target.classList.contains('popup-close')){
+                popup.style.display = 'none';
+            } else {
+                target = target.closest('.popup-content');
+                
+                if(!target){
+                    popup.style.display = 'none';
+                }
+            }
+        });
+
+    };
+
+    togglePopUp();
+
+    //табы
+
+    const tabs = () => {
+        const tabHeader = document.querySelector('.service-header'),
+            tab = tabHeader.querySelectorAll('.service-header-tab'),
+            tabContent = document.querySelectorAll('.service-tab');
+
+        const toggleTabContent = (index) => {
+           for(let i = 0; i < tabContent.length; i++){
+               if(index === i){
+                   tab[i].classList.add('active');
+                   tabContent[i].classList.remove('d-none');
+               } else{
+                tab[i].classList.remove('active');
+                tabContent[i].classList.add('d-none');
+               }
+           } 
+        };
+
+        tabHeader.addEventListener('click', (event) => {
+            let target = event.target;
+                target = target.closest('.service-header-tab');
+
+            if (target){
+                tab.forEach((item, i) => {
+                    if(item === target){
+                        toggleTabContent(i);
+                    }
+                });
+            }
+        });
+
+    };
+
+    tabs();
+
+
 
 });
