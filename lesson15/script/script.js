62e7d5c73e6c617f7bebf5dfcc4a3406e2fe231a
SergeyObraztsov <44734660+SergeyObraztsov@users.noreply.github.com>
2019-10-12 18:27:37 +0300

    task 15

--- lesson15/script/script.js
+++ lesson15/script/script.js
@@ -231,20 +231,58 @@ window.addEventListener('DOMContentLoaded', function(){
 
     slider();
 
-    //RegEx
-    const calculator = () => {
-        const calc = document.querySelectorAll('.calc-item');
+    //calculator
+    const calculator = (price = 100) => {
+        const calc = document.querySelectorAll('.calc-item'),
+            calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
         calc.forEach((elem) => {
             elem.addEventListener('input', (item) => {
                 elem.value = elem.value.replace(/[^\d.]/, '');
             });
         }); 
+
+        const countSum = () => {
+            let total = 0,
+            countValue = 1,
+            dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                 squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1){
+                countValue += ( calcCount.value -1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5){
+                dayValue *= 2;
+            } else if(calcDay.value &&  calcDay.value  < 10){
+                dayValue *= 1.5;
+            }
+
+            if(typeValue && squareValue){
+                total = Math.floor(price * typeValue * squareValue * countValue * dayValue);
+            } else {
+                total = 0;
+            }
+
+            totalValue.textContent = total; 
+        };
+        calcBlock.addEventListener('input', (event) => {
+            const target = event.target;
+            if (target.matches('.calc-item')){
+                countSum();
+            }
+        });
     };
     
-    calculator();
+    calculator(100);
 
     //Photos
-    const changePhoto = () => {
+    const changePhoto = () => { 
         const team = document.querySelector('.command');
             
         team.addEventListener('mouseover', (event) => {
